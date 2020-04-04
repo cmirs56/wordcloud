b47114f3dfd4bbdbead9b43e8db72f72eee7c441
M. Chae <cmirs56@gmail.com>
2020-04-04 17:12:30 +0900

    edit annotation

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-# wordcloud
-wordcloud_practice
+# Description of the source
+* txt file source: Naver (from 1 to 10 page of the web browsing result)
+* Search word: 'corona' 
+* Date: 22nd of March
+
--- corona.R
+++ corona.R
@@ -1,4 +1,7 @@
-library(KoNLP) #loading the required libraries for wordcloud 
+
+# load the necessary libraries for making a wordcloud  --------------------
+
+library(KoNLP)  
 library(wordcloud)
 library(stringr)
 library(XML)
@@ -11,25 +14,28 @@ library(combinat)
 library(multilinguer)
 
 .libPaths()
-useSejongDic() #or useNIADic() 
+useSejongDic() # other dictionary option: useNIADic() 
 
-comments <- readLines("c:/users/MC/R/corona.txt", encoding = "UTF-8")
+
+# load the text file and get all nouns from the sentences -----------------
+
+comments <- readLines("c:/users/MC/R/wordcloud/corona.txt", encoding = "UTF-8") #load the txt file
 head(comments, 10)
 
 comments <- paste0(comments, collapse = "")
 
-nouns <- extractNoun(comments)
-nouns1 <- unlist(nouns)
-nouns2 <- Filter(function(x) {nchar(x) <= 20 & nchar(x) > 1}, nouns1)
+nouns <- extractNoun(comments) # extract nouns from the loaded txt file
+nouns1 <- unlist(nouns) # get the nouns as vectors
+nouns2 <- Filter(function(x) {nchar(x) <= 20 & nchar(x) > 1}, nouns1) # drop one letter or too long nouns
 
-nouns2 <- gsub(" ","", nouns2)
-nouns2 <- gsub("<.+?>|\t", "", nouns2)
+nouns2 <- gsub(" ","", nouns2) # delete the empty spaces, digits, and 
+nouns2 <- gsub("<.+?>|\t", "", nouns2) 
 nouns2 <- gsub("\\d+","" ,nouns2)	
 nouns2 <- gsub("\\.","" ,nouns2) 
 nouns2 <- gsub("\\(","" ,nouns2) 
 nouns2 <- gsub("\\)","" ,nouns2) 
 
-nouns2 <- gsub("코로나","" ,nouns2)
+nouns2 <- gsub("코로나","" ,nouns2) #drop unnecessary nouns 
 nouns2 <- gsub("선정","" ,nouns2)
 nouns2 <- gsub("시간","" ,nouns2)
 nouns2 <- gsub("네이버뉴스","" ,nouns2)
@@ -46,12 +52,19 @@ nouns2 <- gsub("하루","" ,nouns2)
 nouns2 <- gsub("중앙","" ,nouns2)
 nouns2 <- gsub("하기","" ,nouns2)
 nouns2 <- gsub("SBS","" ,nouns2)
-
+nouns2 <- gsub("바이러스","" ,nouns2)
+nouns2 <- gsub("보내기","" ,nouns2)
+nouns2 <- gsub("신종","" ,nouns2)
+nouns2 <- gsub("감염증","" ,nouns2)
+nouns2 <- gsub("국내","" ,nouns2)
 
 nouns3 <- Filter(function(x) {nchar(x) <= 20 & nchar(x) > 1}, nouns2)
 
-wordcount <- table(nouns3)
-head(sort(wordcount, decreasing = TRUE), 50)
+
+# make the wordcloud  -----------------------------------------------------
+
+wordcount <- table(nouns3) # count the frequencies of every single nouns 
+head(sort(wordcount, decreasing = TRUE), 50) 
 
 windowsFonts(malgun=windowsFont("맑은고딕"))
 wordcloud2(data=wordcount, minSize = 5, 
